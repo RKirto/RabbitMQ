985dfa73ab6c8733de466649e0755bb33c77b618
RKirto <r.kirrtadze@gmail.com>
2025-05-16 14:49:47 -0700

    first commit

---
+++ FormulaAirline.API/Controllers/BookingsController.cs
@@ -0,0 +1,26 @@
+﻿using FormulaAirline.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FormulaAirline.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BookingsController : ControllerBase
+    {
+        private readonly ILogger<BookingsController> _logger;
+        private readonly IMessageProducer _messageProducer;
+
+        public BookingsController(ILogger<BookingsController> logger, IMessageProducer messageProducer)
+        {
+            _logger = logger;
+            _messageProducer = messageProducer;
+        }
+
+        [HttpGet]
+        public IActionResult Get() 
+        {
+            return Ok();
+        }
+
+    }
+}
---
+++ FormulaAirline.API/Services/MessageProducer.cs
@@ -0,0 +1,31 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+using System.Text.Json;
+
+namespace FormulaAirline.API.Services
+{
+    public class MessageProducer : IMessageProducer
+    {
+        public async void SendingMessage<T>(T message)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                UserName = "user",
+                Password = "password",
+                VirtualHost = "/"
+            };
+
+            var conn = await factory.CreateConnectionAsync();
+
+            using var channel = await conn.CreateChannelAsync();
+
+            await channel.QueueDeclareAsync("bookings", durable: true, exclusive: true);
+
+            var jsonString = JsonSerializer.Serialize(message);
+            var body = Encoding.UTF8.GetBytes(jsonString);
+
+            await channel.BasicPublishAsync("", "bookings", body: body);
+        }
+    }
+}
