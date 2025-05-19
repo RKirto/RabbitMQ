4215ce3c98e2e4df265ff7ab62455dd0d5c16799
Rati.Kirtadze <rati.kirtadze@tera.ge>
2025-05-19 16:15:04 +0400

    build receiver

--- FormulaAirline.TicketProcessing/Program.cs
+++ FormulaAirline.TicketProcessing/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost",
+    UserName = "user",
+    Password = "password",
+    VirtualHost = "/"
+};
+
+var conn = await factory.CreateConnectionAsync();
+
+using var channel = await conn.CreateChannelAsync();
+
+await channel.QueueDeclareAsync("bookings", durable: true, exclusive: true);
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+
+consumer.ReceivedAsync += async (model, eventArgs) =>
+{
+    // byte[]
+    var body = eventArgs.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+
+    Console.WriteLine($"message has been received - {message}");
+
+    await Task.CompletedTask;
+};
+
+await channel.BasicConsumeAsync("bookings", true, consumer);
+Console.ReadKey();
